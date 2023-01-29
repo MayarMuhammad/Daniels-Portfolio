6b1739e01202b3f05d3bbcd7cf886bf211f759ad
Mayar Muhammed <mayarmuhammad465@gmail.com>
2023-01-29 18:24:29 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+new PureCounter();
+
+function scrollValue() {
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+        navbar.classList.remove('BgColour');
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
+
+window.addEventListener('scroll', scrollValue);
